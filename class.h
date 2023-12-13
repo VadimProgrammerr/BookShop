10c780912dd0fa275c9f9d6941f0238772fccac7
VadimProgrammer <mitske2461@gmail.com>
2023-12-13 11:38:16 +0300

    addBook func

--- class.h
+++ class.h
@@ -1,5 +1,30 @@
 #ifndef CLASS
 #define CLASS
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Book {
+public:
+    Book();
+    ~Book();
+    void menu();
+    string title;
+    string author;
+    double price;
+    int quantity;
+    void addBook();
+private:
+
+    void saleBook();
+    void bookList();
+    void saleList();
+};
+
+
+extern vector<Book> availableBooks;
+
 #endif // CLASS
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <vector>
+#include "class.h"
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Book book;
+    book.addBook();
+    int i=0;
+
+    cout << availableBooks[i].title << " by " << availableBooks[i].author << " - $" << availableBooks[i].price << " (" << availableBooks[i].quantity << " available)" << std::endl;
     return 0;
 }
 
